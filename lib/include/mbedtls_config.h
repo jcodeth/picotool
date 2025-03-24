e0db2437c3b518a7ddbff6688d7f2d611d7d71b5
William Vinnicombe <william.vinnicombe@raspberrypi.com>
2025-03-24 13:23:12 +0000

    Review fixups
    enc_bootloader requires SDK 2.1.2
    
    rename binary_data to embedded_data
    
    move mbedtls TODO into header

--- lib/include/mbedtls_config.h
+++ lib/include/mbedtls_config.h
@@ -15,6 +15,8 @@
 #ifndef MBEDTLS_CONFIG_H
 #define MBEDTLS_CONFIG_H
 
+// todo make the configuration better
+
 #if defined(_MSC_VER) && !defined(_CRT_SECURE_NO_DEPRECATE)
 #define _CRT_SECURE_NO_DEPRECATE 1
 #endif
